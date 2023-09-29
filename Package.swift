4701fe393282202b935e6885d3fe0ae9d60f3bfa
Denis Popov <aoh@list.ru>
2023-09-29 19:07:38 +0300

    changed file name;

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FLAC",
-            url: "https://github.com/de4me/test-package/blob/main/FLAC.zip",
+            url: "https://github.com/de4me/test-package/blob/main/FLAC.xcframework.zip",
             checksum: "265ce97f17d58d8232cc9bbb90d0704b9d417fddb3fbdf09ec1b2f8db5f37d1a")
     ]
 )
