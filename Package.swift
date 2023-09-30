b6e09c116466dcbca2467509c098916b45e8010b
Denis Popov <aoh@list.ru>
2023-10-01 00:02:12 +0300

    1.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FLAC",
-            url: "https://github.com/de4me/test-package/releases/download/1.1/FLAC.xcframework.zip",
-            checksum: "265ce97f17d58d8232cc9bbb90d0704b9d417fddb3fbdf09ec1b2f8db5f37d1a")
+            url: "https://github.com/de4me/test-package/releases/download/1.2/FLAC.zip",
+            checksum: "7fbd26c0caf0ff3975566003b4a1f4a26932f1d49bdc2a80bd17aea2694701e8")
     ]
 )
