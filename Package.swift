7dabd4428964cce13e5426c780d7e60c4be23ccc
Denis Popov <aoh@list.ru>
2023-10-04 20:17:57 +0300

    1.2.7-rc.2

--- Package.swift
+++ Package.swift
@@ -1,23 +1,23 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-// 1.2.6-ver.1
+// 1.2.7-rc.2
 
 import PackageDescription
 
 let package = Package(
-    name: "mpg123",
+    name: "ogg",
     platforms: [
         .macOS(.v10_13), .iOS(.v12)
     ],
     products: [
         .library(
-            name: "mpg123",
-            targets: ["mpg123"]),
+            name: "ogg",
+            targets: ["ogg"]),
     ],
     targets: [
         .binaryTarget(
-            name: "mpg123",
-            url: "https://github.com/de4me/test-package/releases/download/1.2.6-ver.1/mpg123.zip",
-            checksum: "687df44aedce4f56f2d798ded1bec8045a42c8d4d882cac90015eeafd917f37b")
+            name: "ogg",
+            url: "https://github.com/de4me/test-package/releases/download/1.2.7-rc.2/ogg.zip",
+            checksum: "91888a5ca27f400ddd829b43d13d8352041e9f76e7c5b97ee0ee4112bf8af1c1")
     ]
 )
